e94fd708067769491c1219cab5d8496a20d80662
pape sembene <sembenpape4@gmail.com>
2026-02-27 19:12:41 +0000

    chore(swagger): ajouter les dependances et ajuster la configuration prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,34 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaService | undefined;
+}
+
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
-    super({
-      log: ['error'],
-    });
+    // On appelle super UNE SEULE FOIS
+    super({ log: ['error'] });
+
+    // Si le singleton global n'existe pas, on le crée
+    if (!global.prisma) {
+      global.prisma = this;
+    }
+
+    // On retourne le singleton global
+    return global.prisma;
   }
 
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
